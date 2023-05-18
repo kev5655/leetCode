ed3313ce96e8543dfe1ca2e1e1695a20712ef368
Kevin Zahn <kevin.zahn@bluewin.ch>
2023-05-18 13:58:01 +0200

    finish 02AddTowNumbers

--- src/2AddTwoNumbers/index.ts
+++ src/2AddTwoNumbers/index.ts
@@ -1,57 +1,74 @@
-
-
 class ListNode {
-    val: number
-    next: ListNode | null
+    val: number;
+    next: ListNode | null;
     constructor(val?: number, next?: ListNode | null) {
-        this.val = (val===undefined ? 0 : val)
-          this.next = (next===undefined ? null : next)
-      }
- }
- 
+        this.val = val === undefined ? 0 : val;
+        this.next = next === undefined ? null : next;
+    }
+}
 
 function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
-    let listNode: ListNode | null = null;
-    let rest = 0;
+    let result = (l1 ? l1.val : 0) + (l2 ? l2.val : 0);
+    let rest = result >= 10 ? 1 : 0;
+    let curr: ListNode | null = result >= 10 ? new ListNode(result - 10) : new ListNode(result);
+    const listNode: ListNode | null = curr;
 
-    while(l1 !== null || l2 !== null){
-        const result = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + rest;
+    let index = 0;
+    while (l1 !== null || l2 !== null) {
+        l1 = l1 !== null ? l1.next : null;
+        l2 = l2 !== null ? l2.next : null;
 
-        if(result >= 10) {
-            rest = 1
-            listNode = (listNode === null) ? new ListNode(result - 10) :  new ListNode(result -10, listNode)
+        result = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + rest;
+        if (l1 == null && l2 == null && result === 0) break;
+
+        if (result >= 10) {
+            rest = 1;
+            curr.next = new ListNode(result - 10);
         } else {
-            rest = 0
-            listNode = (listNode === null) ? new ListNode(result) :  new ListNode(result, listNode)
+            rest = 0;
+            curr.next = new ListNode(result);
         }
         // console.log(listNode)
 
-        l1 = (l1 !== null) ? l1.next : null;
-        l2 = (l2 !== null) ? l2.next : null;
-
-        if(!(l1 !== null || l2 !== null) && rest !== 0) {
-            listNode = (listNode === null) ? new ListNode(rest) :  new ListNode(rest, listNode)
+        curr = curr.next;
+        index++;
+        console.log(`${index}`);
+        console.log(listNode);
+        if (!(l1 !== null || l2 !== null) && rest !== 0) {
+            curr.next = new ListNode(rest);
         }
-        
     }
-    let temp = null
-    let result = new ListNode(listNode?.val)
-    do {
-        temp = listNode?.val
-        result = new ListNode(temp, result)
-    } while(temp !== null)
 
-    return result;
-};
+    return listNode;
+}
 // const l1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(5, new ListNode(6)))))
 // const l2 = new ListNode(4, new ListNode(5, new ListNode(6, new ListNode(5, new ListNode(6)))))
 
-const l1 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))))))
-const l2 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))))
+// const l1 = new ListNode(
+//     9,
+//     new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))))),
+// );
+// const l2 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))));
+
+const l1 = new ListNode(0);
+const l2 = new ListNode(0);
 
 //[9,9,9,9,9,9,9], l2 = [9,9,9,9]
-let result = addTwoNumbers(l1, l2)
-while(result !== null) {
-    console.log(result.val);
-    result = result.next
-}
+let result = addTwoNumbers(l1, l2);
+console.log(
+    `${result?.val} -> 
+    ${result?.next?.val} -> 
+    ${result?.next?.next?.val} -> 
+    ${result?.next?.next?.next?.val} -> 
+    ${result?.next?.next?.next?.next?.val} -> 
+    ${result?.next?.next?.next?.next?.next?.val} -> 
+    ${result?.next?.next?.next?.next?.next?.next?.val} ->
+    ${result?.next?.next?.next?.next?.next?.next?.next?.val} ->
+    ${result?.next?.next?.next?.next?.next?.next?.next?.next?.val} ->
+    ${result?.next?.next?.next?.next?.next?.next?.next?.next?.next?.val}`,
+);
+
+// while(result !== null) {
+//     console.log(result);
+//     result = result.next
+// }
