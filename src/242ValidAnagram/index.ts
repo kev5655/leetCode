bfba8581d3a73ae7a95a715e79e4d03c80916aff
kevin@fidentity.ch <kevin>
2023-05-19 20:16:05 +0200

    add sort version to 242ValidAnagram

--- src/242ValidAnagram/index.ts
+++ src/242ValidAnagram/index.ts
@@ -1,3 +1,4 @@
+// Time O(s+t) Space maybe O(s+t) with HashMap
 function isAnagram(s: string, t: string): boolean {
     if (s.length !== t.length) return false;
 
@@ -32,3 +33,18 @@ const mapCounter = (store: Map<string, number>, curr: string) => {
 console.log(isAnagram('anagram', 'nagaram'));
 console.log(isAnagram('rat', 'car'));
 console.log(isAnagram('aacc', 'ccac'));
+
+
+// Time O(nlogn) Space O(i) (it depends on the build in sort algo) With Sort
+function isAnagram1(s: string, t: string): boolean {
+    if (s.length !== t.length) return false;
+    const sSorted = s.split('').sort().join()
+    const tSorted = t.split('').sort().join()
+
+    return sSorted === tSorted
+
+}
+
+console.log(isAnagram1('anagram', 'nagaram'));
+console.log(isAnagram1('rat', 'car'));
+console.log(isAnagram1('aacc', 'ccac'));
