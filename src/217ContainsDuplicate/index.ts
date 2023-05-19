9728f706f219d337b1b280bc11c800b7fde55f90
kevin@fidentity.ch <kevin>
2023-05-19 19:52:39 +0200

    add sort version to 217ContainsDuplicate

--- src/217ContainsDuplicate/index.ts
+++ src/217ContainsDuplicate/index.ts
@@ -1,4 +1,4 @@
-// Bad
+// Time O(n2) Space O(i)
 function containsDuplicate(nums: number[]): boolean {
     for (let i = 0; i < nums.length; i++) {
         for (let j = 0; j < nums.length; j++) {
@@ -13,9 +13,19 @@ console.log(containsDuplicate([1, 2, 3, 1]));
 console.log(containsDuplicate([1, 2, 3, 4]));
 
 function containsDuplicate1(nums: number[]): boolean {
+    const sorted = nums.sort();
+    for(let i = 1; i < sorted.length; i++){
+        if(sorted[i-1] === sorted[i]) return true;
+    }
+    return false
+}
+
+
+// Time O(n) Space O(n)
+function containsDuplicate2(nums: number[]): boolean {
     const unique = new Set(nums);
     return unique.size !== nums.length;
 }
 
-console.log(containsDuplicate1([1, 2, 3, 1]));
-console.log(containsDuplicate1([1, 2, 3, 4]));
+console.log(containsDuplicate2([1, 2, 3, 1]));
+console.log(containsDuplicate2([1, 2, 3, 4]));
